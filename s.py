0b4fcde4ba5e9f596ac614916a45e77f7a783760
Yashawanth R <89150253+yashu1wwww@users.noreply.github.com>
2023-11-12 11:41:32 +0530

    Update s.py

--- s.py
+++ s.py
@@ -1,32 +1,37 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
 from time import sleep
 
 driver = webdriver.Chrome()
 driver.get("https://www.instagram.com")
-sleep(3)
-driver.find_element_by_name('username').send_keys('user_find') #replace with your insta username 
-sleep(1)
-driver.find_element_by_name('password').send_keys('seen987') #replace with your insta pass
-sleep(1)
-driver.find_element_by_xpath("//button[@type='submit']").click()
-sleep(10)
-driver.get('https://www.instagram.com/manoj_34_manoj') #replace the username in last after .com
 sleep(5)
-driver.find_element_by_class_name("_abl-").click()#id click
+driver.find_element_by_name('username').send_keys('insta123') #replace with your insta username 
+sleep(1)
+driver.find_element_by_name('password').send_keys('insta123@#') #replace with your insta pass
+sleep(1)
+driver.find_element_by_xpath("//button[@type='submit']").click() #click on submit button
+sleep(6)
+driver.get('https://www.instagram.com/instagram/?hl=en') #replace the username in last after .com
+sleep(5)
+driver.find_element_by_class_name("x1lliihq x1n2onr6 x5n08af").click()  #click on 3 dot button
 sleep(3)
-driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div/button[3]').click()#report
-sleep(3)
-driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div/div/div/div[3]/button[2]/div').click()report account
-sleep(3)
-driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div/div/div/div[1]/button[2]/div').click()#its pretending to soemone
-sleep(3) 
-driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div/div/div/fieldset/div[1]/label/div').click()#me 
-sleep(3)
-driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div/div/div/div[6]').click()#submit report
-sleep(3)
-driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div/div/div/div[4]/button').click()#close
+driver.execute_script('document.querySelector("body > div.x1n2onr6.xzkaem6 > div.x9f619.x1n2onr6.x1ja2u2z > div > div.x1uvtmcs.x4k7w5x.x1h91t0o.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1n2onr6.x1qrby5j.x1jfb8zj > div > div > div > div > div.x7r02ix.xf1ldfh.x131esax.xdajt7p.xxfnqb6.xb88tzc.xw2csxc.x1odjw0f.x5fp0pe > div > div > div > button:nth-child(3)").click()') #report
+sleep(2)
+driver.execute_script('document.querySelector("body > div.x1n2onr6.xzkaem6 > div.x9f619.x1n2onr6.x1ja2u2z > div > div.x1uvtmcs.x4k7w5x.x1h91t0o.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1n2onr6.x1qrby5j.x1jfb8zj > div > div > div > div > div > div > div._acgy > div > div > div > div._ac7r > button:nth-child(2) > div > div.x9f619.xjbqb8w.x78zum5.x168nmei.x13lgxp2.x5pf9jr.xo71vjh.x1n2onr6.x1plvlek.xryxfnj.x1iyjqo2.x2lwn1j.xeuugli.xdt5ytf.xqjyukv.x1qjc9v5.x1oa3qoh.x1nhvcw1").click()') #report account
+sleep(2)
+driver.execute_script('document.querySelector("body > div.x1n2onr6.xzkaem6 > div.x9f619.x1n2onr6.x1ja2u2z > div > div.x1uvtmcs.x4k7w5x.x1h91t0o.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1n2onr6.x1qrby5j.x1jfb8zj > div > div > div > div > div > div > div._acgy > div > div > div > div._ac7r > button:nth-child(2) > div").click()') #its pretending 
+sleep(2) 
+driver.execute_script('document.querySelector("#IGDSRadioButtontag-1").click()') #click on dropdown 
+sleep(2)
+driver.execute_script('document.querySelector("body > div.x1n2onr6.xzkaem6 > div.x9f619.x1n2onr6.x1ja2u2z > div > div.x1uvtmcs.x4k7w5x.x1h91t0o.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1n2onr6.x1qrby5j.x1jfb8zj > div > div > div > div > div > div > div._acgy > div > div > div > div:nth-child(7) > button").click()') #submit report
+#after submitting report it ask "which account is pretending>>give the acc and next and close..
 sleep(20)
